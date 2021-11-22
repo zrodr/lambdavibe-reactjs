87cd7d9dde89feb0c2c7f88bd4c953f11e30fcea
Izaiah Rodriguez <zrodr@protonmail.com>
2021-11-22 12:41:18 -0800

    Created prototype for drums and visualizer

---
+++ client/src/instruments/zrodr.tsx
@@ -0,0 +1,69 @@
+import * as Tone from 'tone';
+import { List } from 'immutable'
+import { useEffect } from 'react';
+
+import { Instrument, InstrumentProps } from '../Instruments';
+
+import '../drumset.css'
+
+interface DrumProps {
+  note: string; // C, D, E, F, G, A, B
+  duration?: string;
+  synth?: Tone.Synth; // Synth for kick and tom sounds
+  name: string;
+}
+
+function Drum({ synth, note, duration, name }: DrumProps) {
+  return (
+    <div 
+      className={`drum-${name}`}
+      onMouseDown={() => synth?.triggerAttackRelease(`${note}`, `${duration}`)}
+    >
+      {name}
+    </div>
+  )
+}
+
+const Drumset = ({ synth, setSynth }: InstrumentProps) => {
+  const drums = List([
+    { name: 'Snare', note: 'C1', duration: '8n' },
+    { name: 'Tom1', note: 'D2', duration: '8n' },
+    { name: 'Tom2', note: 'E1', duration: '8n' },
+    { name: 'FloorTom', note: 'F2', duration: '8n' },
+    { name: 'Bass', note: 'G1', duration: '8n' },
+    //{ name: 'Test1', note: 'A1', duration: '8n' },
+    //{ name: 'Test2', note: 'B1', duration: '8n' },
+  ])
+
+  useEffect(() => {
+    (() => {
+      setSynth(oldSynth => {
+        oldSynth.dispose();
+  
+        return new Tone.MembraneSynth({
+          oscillator: { type: 'square' } as Tone.OmniOscillatorOptions,
+        }).toDestination();
+      });
+    })()
+  }, [setSynth])
+
+  return (
+    <div className="center-container">
+      <div className='container-grid'>
+      {drums.map((drum) => {
+        return (
+          <Drum
+            key={drum.name}
+            note={drum.note}
+            duration={drum.duration}
+            synth={synth}
+            name={drum.name}
+          />
+        )
+      })}
+      </div>
+    </div>
+  )
+}
+
+export const DrumInstrument = new Instrument('Drumset', Drumset)
---
+++ client/src/visualizers/zrodr.tsx
@@ -0,0 +1,47 @@
+import P5 from 'p5';
+import * as Tone from 'tone';
+
+import { Visualizer } from '../Visualizers';
+
+export const CircleVisualizer: Visualizer = new Visualizer(
+  'Circle',
+  (p5: P5, analyzer: Tone.Analyser) => {
+    const width = window.innerWidth;
+    const height = window.innerHeight / 2;
+
+    p5.background('#01274c');
+    p5.noFill();
+    p5.stroke(255, 255, 255, 255);
+    p5.strokeWeight(5);
+    p5.translate(width / 2, height / 2)
+    p5.scale(0.66)
+    
+    p5.angleMode(p5.DEGREES)
+
+    const values = analyzer.getValue()
+
+    p5.beginShape()
+    for (let i = 0; i <= 180; i++) {
+      let idx = p5.floor(p5.map(i, 0, 180, 0, values.length - 1))
+      let radius = p5.map(values[idx] as number, -1, 1, 150, 350)
+
+      const x = radius * p5.sin(i)
+      const y = radius * p5.cos(i)
+
+      p5.vertex(x, y)
+    }
+    p5.endShape()
+    p5.beginShape()
+    for (let i = 0; i <= 180; i++) {
+      let idx = p5.floor(p5.map(i, 0, 180, 0, values.length - 1))
+      let radius = p5.map(values[idx] as number, -1, 1, 150, 350)
+
+      const x = radius * p5.sin(i) * -1
+      const y = radius * p5.cos(i)
+
+      p5.vertex(x, y);
+    }
+    p5.endShape();
+
+  }
+)
