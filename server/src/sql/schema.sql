0fbfd5278e0e4735ee11f94d01045320c7c50e6b
Izaiah Rodriguez <zrodr@protonmail.com>
2021-12-06 10:02:43 -0800

    Added additional songs to db

--- server/src/sql/schema.sql
+++ server/src/sql/schema.sql
@@ -17,3 +17,10 @@ VALUES (1, 'Ode to Joy (Dubstep Remix)', 'E4 E4 F4 G4 G4 F4 E4 D4 C4 C4 D4 E4 E4
 
 INSERT INTO songs (id, song_title, notes)
 VALUES (2, 'In The Air Tonight', 'E1 C2 E2 C1 G1 G1 F1 D2 D2 C1');
+
+INSERT INTO songs (id, song_title, notes) 
+VALUES (3, 'Fure Elise (Beethoven)', 'E4 D4 E4 D4 E4 B4 D4 C4 A4 C4 E4 A4 B4 E4 C4 B4 A4');
+
+INSERT INTO songs (id, song_title, notes) 
+VALUES (4, 'The Anthem', 'G4 C4 G4 A4 B4 E4 E4 A4 G4 F4 G4 C4');
+
